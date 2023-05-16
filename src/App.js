73e208d72afa314d5346d4849a13a588d01b74c2
Ran Pol <rnd809@gmail.com>
2023-05-16 19:55:48 -0400

    Add basic layout components to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from './containers'
+
+import { CTA, Brand, Feature, Navbar } from './components'
+
 const App = () => {
-  return <div>App</div>
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+    </div>
+  )
 }
 
 export default App
